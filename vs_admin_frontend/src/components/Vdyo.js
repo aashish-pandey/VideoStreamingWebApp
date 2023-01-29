dc12e46726bfe2bb7d1fbe4925d6c386310e06dd
sandesh-ghimire <ghimiresandesh45@gmail.com>
2023-01-29 22:38:05 +0530

    last git commit issue resolve

--- vs_admin_frontend/src/components/Vdyo.js
+++ vs_admin_frontend/src/components/Vdyo.js
@@ -22,6 +22,9 @@ const  Vdyo = () => {
 
             <video  width="600" height="400" autoPlay muted id='vdyo2'>
                 <source src="http://media.w3.org/2010/05/sintel/trailer.mp4" type="video/mp4"></source>
+                {/* <source src={URL.createObjectURL(file)} type="video/mp4"></source> */}
+              
+
             </video>
 {/*          
          <video  width="600" height="400" autoPlay muted id='vdyo2'>
