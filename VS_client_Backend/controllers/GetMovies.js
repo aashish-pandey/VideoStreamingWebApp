e8d7feeba6beab049e09fbc3d972bc566439eea2
Aashish Pandey <paashish750@gmail.com>
2023-02-17 18:58:00 +0530

    Latest release data sorted by latest date

--- VS_client_Backend/controllers/GetMovies.js
+++ VS_client_Backend/controllers/GetMovies.js
@@ -1,12 +1,10 @@
 const MovieUpload = require('../models/MovieUpload')
 
-const getMovies = function(req, res){
+const getMovies = async function(req, res){
 
 
-    MovieUpload.find({}, function(err, data){
-        if(err)res.status(500).send({code: 200, msg: "bro error"})
-        else res.status(200).send({code: 200, msg: data})
-    })
+    const data = await MovieUpload.find({}).sort({updatedAt: -1})
+    res.status(200).send({code: 200, msg: data})
 }
 
 module.exports = getMovies;
