13bb31393911e6efb3239e01bf8e49410a096a5b
Aashish Pandey <paashish750@gmail.com>
2023-02-17 16:41:43 +0530

    Trending page backend code added

--- VS_client_Backend/controllers/GetTrendingMovies.js
+++ VS_client_Backend/controllers/GetTrendingMovies.js
@@ -1,7 +1,17 @@
 const WatchHistory = require('../models/WatchHistory')
 
 const getTrendingMovies = function(req, res){
-    res.status(200).send({err: false, msg:"We cool bro?"})
+
+    WatchHistory.distinct("movieId",function(err, details){
+        if(err){
+            res.status(500).send({err:true, msg:err})
+        }else{
+
+            res.status(200).send({err:false, msg:details})
+
+        }
+    })
+
 }
 
 module.exports = getTrendingMovies
