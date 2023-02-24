c83c0c4274bc8cf3f86bbf6b2166094fcbd5a588
Aashish Pandey <paashish750@gmail.com>
2023-02-24 20:10:55 +0530

    Desktop app codebase written and cleaned

---
+++ electron/public/main.js
@@ -0,0 +1,21 @@
+const {app, BrowserWindow} = require('electron')
+require('@electron/remote/main').initialize()
+
+function createWindow(){
+
+const win = new BrowserWindow({
+  width: 800, 
+  height: 600,
+  webPreferences:{
+   enableRemoteModule: true
+}
+
+})
+win.loadURL('http://localhost:3000')
+}
+
+app.on('ready', createWindow)
+
+
+
+
