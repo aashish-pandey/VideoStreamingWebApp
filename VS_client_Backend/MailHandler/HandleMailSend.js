4310ca5e506a7a09f37458657749382bbe7b19ec
bikash <bikashshah565@gmail.com>
2023-04-21 23:56:15 +0530

    backend changes final

--- VS_client_Backend/MailHandler/HandleMailSend.js
+++ VS_client_Backend/MailHandler/HandleMailSend.js
@@ -1,40 +1,46 @@
-const transporter = require('./SendMail')
-const Users = require('../models/Registers')
+const transporter = require("./SendMail");
+const Users = require("../models/Registers");
 
-const HandleMailSend = function(req, res){
+const HandleMailSend = function (req, res) {
+  console.log(req.body.email);
 
-    console.log(req.body.email)
-
-    Users.find({uemail:req.body.email}, function(err, details){
-      if(err){
-        res.status(500).json({code:200, msg:"Internal Server error in fetching records for this email"})
-      }else{
-        if(details.length == 0){
-          res.status(200).json({code:200, msg:"No user with given email found. Try registering this email first"})
-        }else{
+  Users.find({ uemail: req.body.email }, function (err, details) {
+    if (err) {
+      res.status(500).json({
+        code: 200,
+        msg: "Internal Server error in fetching records for this email",
+      });
+    } else {
+      if (details.length == 0) {
+        res.status(200).json({
+          code: 200,
+          msg: "No user with given email found. Try registering this email first",
+        });
+      } else {
         // <a href=\" http://localhost:3560/forgetPasswordBegin/${req.body.email} \">Click me to reset password</a>
-        console.log("reached here1")
-          
-          var mailOptions = {
-              from: 'pandeyaashish100@gmail.com',
-              to: req.body.email,
-              subject: 'JaalChitra Password Reset',
-              html: `<h1>Welcome To Password Reset</h1><form action="http://localhost:3560/forgetPasswordBegin" method="post"><input type="hidden" name="email" value="${req.body.email}" /><input type="submit" value="Click me to reset password"/></form>`
-            };
-            
-            console.log("reached here2")
-            transporter.sendMail(mailOptions, function(error, info){
-              if (error) {
-                console.log(error);
-                res.status(500).json({code:200, msg: "Sending Email failed"})
-              } else {
-                res.status(200).json({code:200, msg: "Check your email to change password"})
-              }
-            });
-        }
+        console.log("reached here1");
+
+        var mailOptions = {
+          from: "pandeyaashish100@gmail.com",
+          to: req.body.email,
+          subject: "JaalChitra Password Reset",
+          html: `<h1>Welcome To Password Reset</h1><form action="http://localhost:3560/forgetPasswordBegin" method="post"><input type="hidden" name="email" value="${req.body.email}" /><input type="submit" style=" padding: 10px 13px 10px 13px; border-radius: 6px; color: white; background-color: red; border:none; cursor: pointer; " value="Click me to reset password"/></form>`,
+        };
+
+        console.log("reached here2");
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+            res.status(500).json({ code: 200, msg: "Sending Email failed" });
+          } else {
+            res
+              .status(200)
+              .json({ code: 200, msg: "Check your email to change password" });
+          }
+        });
       }
-    })
+    }
+  });
+};
 
-}
-
-module.exports = HandleMailSend
+module.exports = HandleMailSend;
--- VS_client_Backend/index.js
+++ VS_client_Backend/index.js
@@ -1,161 +1,136 @@
-var express = require('express')
-const mongoose = require('mongoose')
-var bodyParser = require('body-parser')
-const path = require('path')
-const cors = require('cors')
-const register = require('./controllers/Register')
-const login = require('./controllers/Login')
-var cookieParser = require('cookie-parser')
-const upload = require('express-fileupload')
+var express = require("express");
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const register = require("./controllers/Register");
+const login = require("./controllers/Login");
+var cookieParser = require("cookie-parser");
+const upload = require("express-fileupload");
 
-
-
-const subscriptionStatus = require('./controllers/SubscriptionStatus')
-const planAvailable = require('./controllers/AvailablePlans')
-const activateSubscription = require('./controllers/ActivateSubscription')
-const accountLoginHistorySave = require('./controllers/AccountLoginHistory')
-const logout = require('./controllers/Logout')
-const uploadVideo = require('./controllers/UploadVideos')
-const uploadMovie = require('./controllers/UploadMovies')
-const getMovies = require('./controllers/GetMovies')
-const getVideo = require('./controllers/GetVideo')
-const getThumbnail = require('./controllers/GetThumbnail')
-const saveWatchHistory = require('./controllers/SaveWatchHistory')
-const getWatchHistory = require('./controllers/GetWatchHistory')
-const getMoviesById = require('./controllers/GetMoviesById')
-
+const subscriptionStatus = require("./controllers/SubscriptionStatus");
+const planAvailable = require("./controllers/AvailablePlans");
+const activateSubscription = require("./controllers/ActivateSubscription");
+const accountLoginHistorySave = require("./controllers/AccountLoginHistory");
+const logout = require("./controllers/Logout");
+const uploadVideo = require("./controllers/UploadVideos");
+const uploadMovie = require("./controllers/UploadMovies");
+const getMovies = require("./controllers/GetMovies");
+const getVideo = require("./controllers/GetVideo");
+const getThumbnail = require("./controllers/GetThumbnail");
+const saveWatchHistory = require("./controllers/SaveWatchHistory");
+const getWatchHistory = require("./controllers/GetWatchHistory");
+const getMoviesById = require("./controllers/GetMoviesById");
 
 //admin requests
-const registerAdmin = require('./controllers/RegisterAdmin')
-const loginAdmin = require('./controllers/LoginAdmin')
-const getAllUsers = require('./controllers/GetAllUsers')
-const getAllAdmins = require('./controllers/GetAllAdmins')
-const getAccountLoginHistories = require('./controllers/GetAccountLoginHistories')
-const OnlineStatus = require('./controllers/OnlineStatus')
-const SetInactiveUSersOffline = require('./controllers/SetInactiveUsersOffline')
-const SetHomeAddress = require('./controllers/SetHomeAddress')
-const getMyProfile = require('./controllers/GetMyProfile')
-const getMySubscription = require('./controllers/GetMySubscription')
-const getSubscriptionPlanByName = require('./controllers/GetSubscriptionPlanByName')
-const getTrendingMovies = require('./controllers/GetTrendingMovies')
-const saveGlobalChat = require('./controllers/SaveGlobalChat')
-const getGlobalChat = require('./controllers/GetGlobalChat')
-const deleteAdmin = require('./controllers/AdminSideAdminFuncionalities/DeleteAdmin')
-const deleteMovie = require('./controllers/AdminSideAdminFuncionalities/DeleteMovie')
-const deleteUser = require('./controllers/AdminSideAdminFuncionalities/DeleteUSer')
-const editAdmin = require('./controllers/AdminSideAdminFuncionalities/EditAdmin')
-const changeAdminDepartment = require('./controllers/AdminSideAdminFuncionalities/ChangeAdminDepartment')
-const { sendMail } = require('./MailHandler/SendMail')
-const HandleMailSend = require('./MailHandler/HandleMailSend')
-const bannerUpload = require('./controllers/AdminSideAdminFuncionalities/AddBanner')
-const getBanner = require('./controllers/GetBanner')
-const getBannerThumbnail = require('./controllers/GetBannerThumbnail')
-const getBannerMovie = require('./controllers/GetBannerMovie')
-const forgetPasswordBegin = require('./controllers/ForgetPassword')
-const forgetPasswordComplete = require('./controllers/ForgetPasswordComplete')
+const registerAdmin = require("./controllers/RegisterAdmin");
+const loginAdmin = require("./controllers/LoginAdmin");
+const getAllUsers = require("./controllers/GetAllUsers");
+const getAllAdmins = require("./controllers/GetAllAdmins");
+const getAccountLoginHistories = require("./controllers/GetAccountLoginHistories");
+const OnlineStatus = require("./controllers/OnlineStatus");
+const SetInactiveUSersOffline = require("./controllers/SetInactiveUsersOffline");
+const SetHomeAddress = require("./controllers/SetHomeAddress");
+const getMyProfile = require("./controllers/GetMyProfile");
+const getMySubscription = require("./controllers/GetMySubscription");
+const getSubscriptionPlanByName = require("./controllers/GetSubscriptionPlanByName");
+const getTrendingMovies = require("./controllers/GetTrendingMovies");
+const saveGlobalChat = require("./controllers/SaveGlobalChat");
+const getGlobalChat = require("./controllers/GetGlobalChat");
+const deleteAdmin = require("./controllers/AdminSideAdminFuncionalities/DeleteAdmin");
+const deleteMovie = require("./controllers/AdminSideAdminFuncionalities/DeleteMovie");
+const deleteUser = require("./controllers/AdminSideAdminFuncionalities/DeleteUSer");
+const editAdmin = require("./controllers/AdminSideAdminFuncionalities/EditAdmin");
+const changeAdminDepartment = require("./controllers/AdminSideAdminFuncionalities/ChangeAdminDepartment");
+const { sendMail } = require("./MailHandler/SendMail");
+const HandleMailSend = require("./MailHandler/HandleMailSend");
+const bannerUpload = require("./controllers/AdminSideAdminFuncionalities/AddBanner");
+const getBanner = require("./controllers/GetBanner");
+const getBannerThumbnail = require("./controllers/GetBannerThumbnail");
+const getBannerMovie = require("./controllers/GetBannerMovie");
+const forgetPasswordBegin = require("./controllers/ForgetPassword");
+const forgetPasswordComplete = require("./controllers/ForgetPasswordComplete");
 
+app = express();
 
-app = express()
+app.use(upload());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(upload())
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser())
-app.use(cors())
-app.use(bodyParser.json())
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs')
+const dbURI =
+  "mongodb+srv://aashish:aashish@cluster0.ue3jr5n.mongodb.net/Users?retryWrites=true&w=majority";
 
+mongoose
+  .connect(dbURI)
+  .then(() =>
+    app.listen(3560, function () {
+      console.log("Authentication system is running");
+    })
+  )
+  .catch((err) => console.log(err));
 
-const dbURI = 'mongodb+srv://aashish:aashish@cluster0.ue3jr5n.mongodb.net/Users?retryWrites=true&w=majority';
+app.get("/getMovies", getMovies);
+app.get("/getBanner", getBanner);
+app.get("/getVideo/:id", getVideo);
+app.get("/getThumbnail/:id", getThumbnail);
+app.get("/getBannerThumbnail/:id", getBannerThumbnail);
 
-mongoose.connect(dbURI)
-    .then(()=>
-        app.listen(3560, function(){
-            console.log("Authentication system is running")
-        })
-    )
-    .catch((err)=>console.log(err))
+app.get("/getMoviesByID/:id", getMoviesById);
+app.get("/getBannerMovie/:id", getBannerMovie);
 
+app.post("/forgetPasswordBegin/", forgetPasswordBegin);
+app.post("/forgetPasswordComplete", forgetPasswordComplete);
 
+app.get("/getMyProfile/:email", getMyProfile);
+app.get("/getMySubscription/:email", getMySubscription);
+app.get("/getSubscriptionPlanByName/:name", getSubscriptionPlanByName);
+app.get("/getTrendingMovies", getTrendingMovies);
+app.get("/getGlobalChat", getGlobalChat);
 
-    
-app.get('/getMovies', getMovies)
-app.get('/getBanner', getBanner)
-app.get('/getVideo/:id', getVideo)
-app.get('/getThumbnail/:id', getThumbnail)
-app.get('/getBannerThumbnail/:id', getBannerThumbnail)
+app.post("/sendMail", HandleMailSend);
 
-app.get('/getMoviesByID/:id', getMoviesById)
-app.get('/getBannerMovie/:id', getBannerMovie)
+app.post("/register", register);
+app.post("/login", login);
+app.post("/logout", logout);
+app.post("/subscriptionStatus", subscriptionStatus);
+app.post("/availablePlan", planAvailable);
+app.post("/activatesubscription", activateSubscription);
+app.post("/accountLoginHistorySave", accountLoginHistorySave);
+app.post("/uploadvideo", uploadVideo);
+app.post("/uploadMovie", uploadMovie);
+app.post("/bannerUpload", bannerUpload);
+app.post("/saveWatchHistory", saveWatchHistory);
+app.post("/getWatchHistory", getWatchHistory);
+app.post("/saveGlobalChat", saveGlobalChat);
 
-app.post('/forgetPasswordBegin/', forgetPasswordBegin)
-app.post('/forgetPasswordComplete', forgetPasswordComplete)
-
-app.get('/getMyProfile/:email', getMyProfile)
-app.get('/getMySubscription/:email', getMySubscription)
-app.get('/getSubscriptionPlanByName/:name', getSubscriptionPlanByName)
-app.get('/getTrendingMovies', getTrendingMovies)
-app.get('/getGlobalChat', getGlobalChat)
-
-
-
-app.post('/sendMail', HandleMailSend)
-
-
-
-app.post('/register', register)
-app.post('/login', login)
-app.post('/logout', logout)
-app.post('/subscriptionStatus', subscriptionStatus)
-app.post('/availablePlan', planAvailable)
-app.post('/activatesubscription', activateSubscription)
-app.post('/accountLoginHistorySave', accountLoginHistorySave)
-app.post('/uploadvideo', uploadVideo)
-app.post('/uploadMovie', uploadMovie)
-app.post('/bannerUpload', bannerUpload)
-app.post('/saveWatchHistory', saveWatchHistory)
-app.post('/getWatchHistory', getWatchHistory)
-app.post('/saveGlobalChat', saveGlobalChat)
-
-
-app.get('/accountLoginHistories/:email', getAccountLoginHistories)
+app.get("/accountLoginHistories/:email", getAccountLoginHistories);
 
 //set Home Address
-app.post('/setHomeAddress', SetHomeAddress)
-
+app.post("/setHomeAddress", SetHomeAddress);
 
 //admin requests
-app.post('/registerAdmin', registerAdmin)
-app.post('/loginAdmin', loginAdmin)
-app.post('/deleteAdmin',deleteAdmin)
-app.post('/deleteMovie', deleteMovie)
-app.post('/deleteUser', deleteUser)
-app.post('/editAdmin', editAdmin)
-app.post('/changeAdminDepartment', changeAdminDepartment)
+app.post("/registerAdmin", registerAdmin);
+app.post("/loginAdmin", loginAdmin);
+app.post("/deleteAdmin", deleteAdmin);
+app.post("/deleteMovie", deleteMovie);
+app.post("/deleteUser", deleteUser);
+app.post("/editAdmin", editAdmin);
+app.post("/changeAdminDepartment", changeAdminDepartment);
 
-
-app.get('/getAllUsers', getAllUsers)
-app.get('/getAllAdmins', getAllAdmins)
-app.get('/getAllMovies', getMovies)
-app.get('/getAllSubscriptionPlans', planAvailable)
-
-
-
-
-
+app.get("/getAllUsers", getAllUsers);
+app.get("/getAllAdmins", getAllAdmins);
+app.get("/getAllMovies", getMovies);
+app.get("/getAllSubscriptionPlans", planAvailable);
 
 //maintaing the active login status
-app.post("/saveOnlineStatus", OnlineStatus)
-
-
+app.post("/saveOnlineStatus", OnlineStatus);
 
 //Automatically make inactive users offline
-setInterval(SetInactiveUSersOffline, 10000)
-
-
-
-
-
+setInterval(SetInactiveUSersOffline, 10000);
 
 // add new subscription plans
 // const Plan = require('./models/PlanAvailable')
@@ -178,5 +153,3 @@ setInterval(SetInactiveUSersOffline, 10000)
 //     })
 
 // });
-
-
